02820b05a4fe71dc43c9ad4fe11c4305923c8167
kawalec <kawalec@gmail.com>
2020-12-18 12:34:16 +0100

    Add data and state

--- src/App.js
+++ src/App.js
@@ -7,8 +7,59 @@ import Board from "./components/Board/Board";
 import Dice from "./components/Dice/Dice";
 import Legend from "./components/Legend/Legend";
 
+const fields = [
+  {
+    id: 0,
+    name: "A",
+  },
+  {
+    id: 1,
+    name: "B",
+  },
+  {
+    id: 2,
+    name: "C",
+  },
+  {
+    id: 3,
+    name: "D",
+  },
+];
+// get from backend
+const specialFields = [
+  { id: 13, effect: 0, bcg: "red", description: "Game Over!" },
+  {
+    id: 24,
+    effect: 12,
+    bcg: "blue",
+    description: "Go to field number 12.",
+  },
+];
 class App extends Component {
-  state = {};
+  state = {
+    dice: ["A", "B", "C", "D", "STOP", "-1"],
+    rolledDice: "START",
+    win: null,
+    // get from backend
+    players: [
+      {
+        id: 1,
+        name: "Paweł",
+        activeFieldId: 1,
+        diceRollsSum: 0,
+        diceRollsFields: [],
+        activeTurn: true,
+      },
+      {
+        id: 2,
+        name: "Agnieszka",
+        activeFieldId: 1,
+        diceRollsSum: 0,
+        diceRollsFields: [],
+        activeTurn: false,
+      },
+    ],
+  };
   render() {
     return <Board />;
   }
